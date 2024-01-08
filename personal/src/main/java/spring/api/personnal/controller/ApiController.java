1a66fe5f68f690b55eba7ecb7d6afc3575d9934d
Phongsathon Wandee <phongsathon@sirisoft.co.th>
2024-01-08 16:41:37 +0700

    [enhance][api]

--- personal/src/main/java/spring/api/personnal/controller/ApiController.java
+++ personal/src/main/java/spring/api/personnal/controller/ApiController.java
@@ -4,13 +4,26 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.api.personnal.models.UserModel;
+import spring.api.share.consts.ApiConsts;
+import spring.api.share.response.ShareResponse;
 
 @RestController
 @RequestMapping("api/test")
 @CrossOrigin(origins = "*")
 public class ApiController {
     @GetMapping("/")
-    public String getStart() {
-        return "Pass";
+    public ShareResponse getResponse(){
+        ShareResponse  response = new ShareResponse();
+        return response.Success();
+    }
+
+    @GetMapping("/data")
+    public ShareResponse getResponseWithData(){
+        ShareResponse  response = new ShareResponse();
+        UserModel user1 = new UserModel();
+        user1.setName("Book");
+        user1.setAge(20);
+        return response.SuccessWithData(user1);
     }
 }
---
+++ personal/src/main/java/spring/api/personnal/models/UserModel.java
@@ -0,0 +1,9 @@
+package spring.api.personnal.models;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String name;
+    private int age;
+}
